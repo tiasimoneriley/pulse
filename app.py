58821a42f874d57e3be34e87a1098ae0c45b6185
patrykk <paddy.szafranski@gmail.com>
2021-04-11 02:47:42 +0100

    ini

--- app.py
+++ app.py
@@ -1,33 +1,39 @@
-from flask import Flask, render_template, request
-from flask_sockets import Sockets
-import os
-import model
 import json
+import os
+
+from flask import Flask, render_template
+from flask_sockets import Sockets
+
+import model
 
 app = Flask(__name__)
 sockets = Sockets(app)
 
+
 @app.route("/")
 def index():
-	return render_template("splash.html")
+    return render_template("splash.html")
+
 
 @app.route("/begin")
 def get_heartrate():
-	return render_template("index.html")
+    return render_template("index.html")
 
 
 @sockets.route('/echo')
 def echo_socket(ws):
-	while True:
-		message = json.loads(ws.receive())
-		signals = model.parse_RGB(message)
-		
-		ws.send(signals)
+    while True:
+        message = json.loads(ws.receive())
+        signals = model.parse_RGB(message)
+
+        ws.send(signals)
+
 
 if __name__ == "__main__":
-	from gevent import pywsgi
-	from geventwebsocket.handler import WebSocketHandler
-	port = int(os.environ.get('PORT', 5000))
-	print("Hosting on port {}".format(port))
-	server = pywsgi.WSGIServer(('', port), app, handler_class=WebSocketHandler)
-	server.serve_forever()
+    from gevent import pywsgi
+    from geventwebsocket.handler import WebSocketHandler
+
+    port = int(os.environ.get('PORT', 5000))
+    print("Hosting on port {}".format(port))
+    server = pywsgi.WSGIServer(('', port), app, handler_class=WebSocketHandler)
+    server.serve_forever()
